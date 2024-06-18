6ba92b560f914256ea8c1585f6eaf5c004c2e4ad
Aman-Abhishek-18 <70442464+Aman-Abhishek-18@users.noreply.github.com>
2024-06-18 22:44:58 +0530

    Update Product Sales Per City.sql

--- Product Sales Per City.sql
+++ Product Sales Per City.sql
@@ -1,5 +1,4 @@
-SELECT ci.city_name, pr.product_name, ROUND(SUM(ii.line_total_price), 2) AS tot
-FROM city ci, customer cu, invoice i, invoice_item ii, product pr 
-WHERE ci.id = cu.city_id AND cu.id = i.customer_id AND i.id = ii.invoice_id AND ii.product_id = pr.id 
-GROUP BY ci.city_name, pr.product_name 
-ORDER BY tot DESC, ci.city_name, pr.product_name 
+SELECT c.city_name, pro.product_name, ROUND(SUM(ii.line_total_price), 2) AS total
+FROM city c, customer cu, invoice i, invoice_item ii, product pro 
+WHERE c.id = cu.city_id AND cu.id = i.customer_id AND i.id = ii.invoice_id AND ii.product_id = pro.id 
+GROUP BY c.city_name, pro.product_name  ORDER BY total DESC, c.city_name, pro.product_name 
